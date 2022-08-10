fad1c20e666cb06517cbb4911d4864572866fa32
haemolysis <trxnsplxnt@gmail.com>
2022-08-10 12:14:10 +1000

    new functions, startup messages

--- EXO-Functions.psm1
+++ EXO-Functions.psm1
@@ -1,6 +1,21 @@
 <#
  The Brains Trust Dev Department presents.. 
 #>
+Write-Host @"
+
+`$`$`$`$`$`$`$`$\ `$`$\   `$`$\  `$`$`$`$`$`$\        `$`$\   `$`$\           `$`$\                               
+`$`$  _____|`$`$ |  `$`$ |`$`$  __`$`$\       `$`$ |  `$`$ |          `$`$ |                              
+`$`$ |      \`$`$\ `$`$  |`$`$ /  `$`$ |      `$`$ |  `$`$ | `$`$`$`$`$`$\  `$`$ | `$`$`$`$`$`$\   `$`$`$`$`$`$\   `$`$`$`$`$`$\  
+`$`$`$`$`$\     \`$`$`$`$  / `$`$ |  `$`$ |      `$`$`$`$`$`$`$`$ |`$`$  __`$`$\ `$`$ |`$`$  __`$`$\ `$`$  __`$`$\ `$`$  __`$`$\ 
+`$`$  __|    `$`$  `$`$<  `$`$ |  `$`$ |      `$`$  __`$`$ |`$`$`$`$`$`$`$`$ |`$`$ |`$`$ /  `$`$ |`$`$`$`$`$`$`$`$ |`$`$ |  \__|
+`$`$ |      `$`$  /\`$`$\ `$`$ |  `$`$ |      `$`$ |  `$`$ |`$`$   ____|`$`$ |`$`$ |  `$`$ |`$`$   ____|`$`$ |      
+`$`$`$`$`$`$`$`$\ `$`$ /  `$`$ | `$`$`$`$`$`$  |      `$`$ |  `$`$ |\`$`$`$`$`$`$`$\ `$`$ |`$`$`$`$`$`$`$  |\`$`$`$`$`$`$`$\ `$`$ |      
+\________|\__|  \__| \______/       \__|  \__| \_______|\__|`$`$  ____/  \_______|\__|      
+                                                            `$`$ |                          
+                                                            `$`$ |  MADE BY MiCHAEL                        
+                                                            \__|                                                                                                   
+                                 
+"@
 
 #cheeky hint to upgrade to PS7
 if ($PSVersionTable.PSVersion -like "6.*" -or $PSVersionTable.PSVersion -like "5.*") {
@@ -35,9 +50,23 @@ if (!(Test-Path $env:USERPROFILE\Documents\temp\accounts.txt)) {
     Write-Warning "A blank accounts.txt file has been created in the above location for you"
 }
 
+Write-Host "Lost? Need help? Find commands with Get-Command -Module EXOP-Functions or Show-EXOHelp"
+
 #set  accounts variable to accounts.txt - probs shouldn't set global variables but w/e, used script
 $Script:accounts = "$env:USERPROFILE\Documents\temp\accounts.txt"
 $Script:tempdir = "$env:USERPROFILE\Documents\temp"
+
+#show commands in this module
+
+function Show-EXOHelp {
+    Write-Host @"
+    List of commands: 
+    Show-EXOHelp                            : Shows this help
+    Show-DynamicDistributionGroupMembers    : Show members of a dynamic DL
+    Run-PreflightChecks                     : Check validity of data set for list of users
+"@
+    
+}
 
 #list memebers of a dynamic DL - can't see this in Exchange Online for some reason
 function Show-DynamicDistributionGroupMembers {
@@ -49,5 +78,48 @@ function Show-DynamicDistributionGroupMembers {
 
 Set-Alias -Name Show-DDGM -Value Show-DynamicDistributionGroupMembers #create alias
 
+#check validity of user data sets, probably a better way of doing this but w/e, looks nice
+function Show-PreflightChecks {
+    $bad = @()
+    $i = 0
+
+    foreach ($user in $accounts) {
+        Write-Host "Checking $user"
+        try {
+            Get-EXOMailbox -Identity $user | Out-Null
+        }
+        catch {
+            Write-Host "User $user does not exist, please check the entry" -ForegroundColor Red
+            $bad += $user
+        }
+        finally {
+            $i = $i + 1
+            Write-Progress -Activity "checking users" -Status "Progress: " -PercentComplete ($i / $users.count * 100)
+        }
+
+        if ($bad) {
+            Write-Host -ForegroundColor Red @"
+            +++++++++++++++++++++++++
+            +                       +                       
+            +      BAD  USERS       +
+            +       DETECTED        +
+            +                       +
+            +++++++++++++++++++++++++
+"@
+            foreach ($u in $bad) {
+                Write-Host -ForegroundColor Red "$u"
+            }
+        }
+        else {
+            Write-Host -ForegroundColor Green @"
++++++++++++++++++++++++++
++                       +                       
++     NO BAD USERS      +
++                       +
++++++++++++++++++++++++++
+"@
+        }
+    }
+}
 
 Export-ModuleMember -Function * -Alias *
